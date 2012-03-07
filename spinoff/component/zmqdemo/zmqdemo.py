5d755c9b68238a71d0fb1de5785f099fdad79f27
Erik Allik <erik.allik@skype.net>
2012-03-07 12:49:48 +0200

    Added zmqdemo and zmqdemo2 for demoing/testing the ZeroMQ transport components

---
+++ spinoff/component/zmqdemo/zmqdemo.py
@@ -0,0 +1,67 @@
+import random
+
+from twisted.internet.defer import inlineCallbacks
+from txzmq import ZmqFactory
+
+from spinoff.component.component import Component, Application, Pipeline
+from spinoff.component.transport.zeromq import ZmqReq, ZmqRep
+from spinoff.util.async import sleep
+
+
+class ZmqTestClient(Component):
+
+    @inlineCallbacks
+    def start(self):
+        while True:
+            for i in range(3):
+                self.put('elloo%d' % i)
+            for i in range(3):
+                msg = yield self.get()
+                print 'got message: ', msg
+            yield sleep(3.0 + random.random())
+
+
+class ZmqTestServer(Component):
+
+    @inlineCallbacks
+    def start(self):
+        while True:
+            req = yield self.get()
+            print 'got request: ', req
+            yield sleep(5.0)
+            self.put(req)
+
+
+f = ZmqFactory()
+
+
+client1 = ZmqTestClient()
+client2 = ZmqTestClient()
+client3 = ZmqTestClient()
+server = ZmqTestServer()
+
+application = Application(
+    Pipeline(
+        client1,
+        ZmqReq(f, 'ipc://foobar', 'client1'),
+        client1,
+        ),
+
+    Pipeline(
+        client2,
+        ZmqReq(f, 'ipc://foobar', 'client2'),
+        client2,
+        ),
+
+    Pipeline(
+        client3,
+        ZmqReq(f, 'ipc://foobar', 'client3'),
+        client3,
+        ),
+
+    Pipeline(
+        server,
+        ZmqRep(f, 'ipc://foobar'),
+        server,
+        ),
+    )
