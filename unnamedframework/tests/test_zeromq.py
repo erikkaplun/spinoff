7a036d924168ef23d106ff6da469ef334d0fc60f
Erik Allik <erik.allik@skype.net>
2012-03-29 00:44:25 +0200

    Added the first test for the ZeroMQ transport components; this one tests 1*ROUTER->1*DEALER

---
+++ unnamedframework/tests/test_zeromq.py
@@ -0,0 +1,43 @@
+from twisted.internet.defer import inlineCallbacks
+from twisted.trial import unittest
+from txzmq import ZmqFactory
+
+from unnamedframework.component.component import Component
+from unnamedframework.component.transport.zeromq import ZmqRouter, ZmqDealer
+from unnamedframework.util.async import TimeoutError, sleep, with_timeout
+from unnamedframework.util.testing import assert_not_raises
+
+
+_wait_for_msg = lambda d: with_timeout(0.2, d)
+_wait_slow_joiners = lambda: sleep(0.0)
+
+
+ADDR = 'ipc://test'
+
+
+class Mock(Component):
+    pass
+
+
+class TestCase1(unittest.TestCase):
+
+    @inlineCallbacks
+    def setUp(self):
+        self.mock = Mock()
+        f = ZmqFactory()
+        self.z_router = ZmqRouter(f, ('bind', ADDR))
+        self.z_dealer = ZmqDealer(f, ('connect', ADDR), identity='dude')
+        self.z_dealer.connect('default', self.mock)
+        yield _wait_slow_joiners()
+
+    @inlineCallbacks
+    def test_router(self):
+        self.z_router.deliver(message=('dude', 'PING'), inbox='default')
+
+        with assert_not_raises(TimeoutError, "should have received a message"):
+            msg = yield _wait_for_msg(self.mock.get('default'))
+        assert msg == 'PING'
+
+    def tearDown(self):
+        self.z_dealer.stop()
+        self.z_router.stop()
