aa18b6b5c7a9a2831b825b1cc805f4a4977af875
Erik Allik <erik.allik@skype.net>
2012-05-16 16:30:36 +0300

    Added Component.get_routed and test_actor

---
+++ spinoff/tests/test_actor.py
@@ -0,0 +1,31 @@
+from spinoff.component.component import Component, InterfaceException
+from spinoff.util.testing import deferred_result, assert_not_raises, assert_raises
+
+
+def test_basic():
+    c = Component()
+    mock = Component()
+    c.connect('default', ('default', mock))
+
+    c.put(message='whatev')
+    with assert_not_raises():
+        deferred_result(mock.get())
+
+    c.put(message='msg-1', routing_key=123)
+    with assert_raises(InterfaceException):
+        deferred_result(mock.get())
+
+    c.put(message='msg-1', routing_key=123)
+    with assert_not_raises(InterfaceException):
+        routing_key, msg = deferred_result(mock.get_routed())
+    assert msg == 'msg-1'
+    assert routing_key == 123
+
+    c.put(message='msg-2', routing_key=321)
+    routing_key, msg = deferred_result(mock.get_routed())
+    assert msg == 'msg-2'
+    assert routing_key == 321
+
+    c.put(message='whatev')
+    with assert_raises(InterfaceException):
+        deferred_result(mock.get_routed())
