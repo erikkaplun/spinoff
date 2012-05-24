60e6e015cdbd0b3c0388dd8148f76d9371c7c692
Erik Allik <erik.allik@skype.net>
2012-05-24 20:25:24 +0300

    Removed actor.util.Filter

--- unnamedframework/actor/util.py
+++ unnamedframework/actor/util.py
@@ -28,22 +28,3 @@ class ComponentCollection(object):
             args, kwargs = connection
             for component in components:
                 component.connect(*args, **kwargs)
-
-
-class Filter(ComponentCollection):
-    """Filters messages based on `routing_key`.
-
-    The function that maps members to routing key values needs to be provided in the constructor.
-
-    """
-    def __init__(self, key_fun, *args, **kwargs):
-        super(Filter, self).__init__(*args, **kwargs)
-        self._key_fun = key_fun
-
-    @inlineCallbacks
-    def deliver(self, inbox, message, routing_key):
-        ds = []
-        for member in self._members:
-            if self._key_fun(member) == routing_key:
-                ds.append(member.deliver(inbox, message, routing_key))
-        yield DeferredList(ds, consumeErrors=True, fireOnOneErrback=True)
