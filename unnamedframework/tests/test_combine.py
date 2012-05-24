7adc82c122715983817c3f2704e13b2fb2650986
Erik Allik <erik.allik@skype.net>
2012-05-24 19:54:31 +0300

    Added a unit test for util.async.combine

---
+++ unnamedframework/tests/test_combine.py
@@ -0,0 +1,30 @@
+from twisted.internet.defer import succeed, fail
+from twisted.python.failure import Failure
+from twisted.trial import unittest
+
+from unnamedframework.util.async import combine
+from unnamedframework.util.testing import errback_called, callback_called
+
+
+class CombineTestCase(unittest.TestCase):
+
+    def test_basic(self):
+        f = lambda msg=None: fail(Failure(Exception(msg)))
+        d = combine()
+        assert d.called, "An empty combine should return an already fired deferred when no deferreds were passed to it"
+
+        d = combine(succeed(None))
+        assert callback_called(d), "ombine should return an already fired deferred when a success deferred was passed to it"
+
+        d = combine(f())
+        assert errback_called(d)
+
+        d = combine(succeed(None), f())
+        assert errback_called(d)
+
+        d = combine(f(), succeed(None))
+        assert errback_called(d)
+
+        d = combine(succeed(None), succeed(None), succeed(None), succeed(None))
+        assert not errback_called(d)
+        assert callback_called(d)
