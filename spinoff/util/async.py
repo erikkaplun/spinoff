baca9b3b9966cec508fdcecb6c9ab1606a2fac64
Erik Allik <erik.allik@skype.net>
2012-05-25 13:25:34 +0300

    Made CancelledError also explicitly importable from util.async

--- spinoff/util/async.py
+++ spinoff/util/async.py
@@ -3,7 +3,7 @@ from twisted.internet.defer import inlineCallbacks, Deferred, TimeoutError, Canc
 from twisted.internet import reactor, task
 
 
-__all__ = ['TimeoutError', 'sleep', 'exec_async', 'if_', 'with_timeout', 'combine']
+__all__ = ['TimeoutError', 'sleep', 'exec_async', 'if_', 'with_timeout', 'combine', 'CancelledError']
 
 
 def sleep(seconds, reactor=reactor):
