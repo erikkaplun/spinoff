34f772982bba1aa45c524500bb33c57f1159dd7d
Erik Allik <erik.allik@skype.net>
2012-05-29 14:25:24 +0200

    Extracted the Relay device from DTS

---
+++ spinoff/actor/device/relay.py
@@ -0,0 +1,81 @@
+from twisted.internet import reactor
+from twisted.internet.defer import inlineCallbacks
+
+from spinoff.actor import Actor, RoutingException, InterfaceException
+from spinoff.util.async import sleep, CancelledError
+
+
+class Relay(Actor):
+
+    def __init__(self, max_message_age=None, reactor=reactor, cleanup_interval=60, *args, **kwargs):
+        super(Relay, self).__init__(*args, **kwargs)
+        self._nodes = {}
+        self._node_addrs = {}
+        self._pending = {}
+        self._max_message_age = max_message_age
+        self._reactor = reactor
+        self._cleanup_interval = (cleanup_interval if max_message_age is None
+                                  else min(max_message_age, cleanup_interval))
+        self._stopped = False
+
+    @inlineCallbacks
+    def start(self):
+        while True:
+            currtime = self._reactor.seconds()
+            tmp = {}
+            for participant_id, pending in self._pending.items():
+                pending = [(m, c) for (m, c) in pending if 0 and c > currtime - self._max_message_age]
+                if pending:
+                    tmp[participant_id] = pending
+            self._pending = tmp
+            self._cleaning_d = sleep(self._cleanup_interval, reactor=self._reactor)
+            try:
+                yield self._cleaning_d
+            except CancelledError:
+                break
+
+    def stop(self):
+        self._cleaning_d.cancel()
+
+    def deliver(self, message, inbox):
+        if not isinstance(message, tuple) and len(message) >= 2:
+            raise RoutingException("Messages to Relay should be tuples whose first element is the sender ID")
+        if inbox != 'messages':
+            raise InterfaceException("Relay only has inbox 'messages'")
+
+        if len(message) != 2:
+            raise InterfaceException("messages should be 2-tuples")
+        sender_id, (command, args) = message
+        # command, args = message
+
+        if command == 'init':
+            participant_id, = args
+            if participant_id is None:
+                raise InterfaceException("Initialization messages should contain the participant ID")
+            if sender_id in self._nodes:
+                raise InterfaceException("Node already registered")
+
+            self._nodes[sender_id] = participant_id
+            self._node_addrs[participant_id] = sender_id
+
+            if participant_id in self._pending:
+                currtime = self._reactor.seconds()
+                for message, created_at in self._pending[participant_id]:
+                    if self._max_message_age is None or created_at > currtime - self._max_message_age:
+                        self.put(outbox='messages', message=(sender_id, message))
+                del self._pending[participant_id]
+        elif command == 'uninit':
+            if sender_id not in self._nodes:
+                raise RoutingException("Node not registered")
+            del self._node_addrs[self._nodes[sender_id]]
+            del self._nodes[sender_id]
+        elif command == 'send':
+            payload, recipient = args
+            if recipient is None:
+                raise InterfaceException()
+            if recipient in self._node_addrs:
+                self.put(outbox='messages', message=(self._node_addrs[recipient], payload))
+            else:
+                self._pending.setdefault(recipient, []).append((payload, self._reactor.seconds()))
+        else:
+            raise InterfaceException("No such command: %s" % command)
---
+++ spinoff/tests/test_relay.py
@@ -0,0 +1,95 @@
+from twisted.internet import reactor
+from twisted.internet.task import Clock
+from twisted.trial import unittest
+
+from spinoff.actor import IActor, RoutingException, Actor, InterfaceException
+from spinoff.util.testing import assert_raises, assert_not_raises, deferred_result
+
+from dts.server.device.relay import Relay
+
+
+class HttpGatewayTest(unittest.TestCase):
+
+    def setUp(self):
+        self._create_relay()
+
+    def _create_relay(self, use_clock=False, **kwargs):
+        self.clock = Clock() if use_clock else None
+        self.relay = Relay(reactor=self.clock if use_clock else reactor, **kwargs)
+        self.mock = Actor()
+        self.relay.connect('messages', ('default', self.mock))
+
+        self.relay.start()
+        self.addCleanup(self.relay.stop)
+
+    def test_interface(self):
+        x = self.relay
+
+        assert IActor.providedBy(x)
+
+        with assert_raises(InterfaceException):
+            x.deliver(message=(1, ('send', ('whatev', 2))), inbox='default')
+
+        with assert_raises(InterfaceException):
+            x.deliver(message=(1, ('send', ('whatev', None))), inbox='messages')
+        with assert_raises(InterfaceException):
+            x.deliver(message=('send', ('whatev', 1)), inbox='messages')
+
+        x.connect('messages', ('default', Actor()))
+        with assert_not_raises(RoutingException):
+            x.deliver(message=(1, ('send', ('whatev', 1))), inbox='messages')
+
+        with assert_raises(InterfaceException):
+            x.deliver(message=(1, ('init', (None, ))), inbox='messages')
+        with assert_not_raises(InterfaceException):
+            x.deliver(message=(1, ('init', ('some-id', ))), inbox='messages')
+
+    def test_delivery(self):
+        x = self.relay
+        mock = self.mock
+
+        x.deliver(message=('node-1', ('init', [1])), inbox='messages')
+        x.deliver(message=('node-2', ('send', ['msg-1', 1])), inbox='messages')
+
+        _, msg = deferred_result(mock.get())
+        assert msg == 'msg-1'
+
+        x.deliver(message=('node-2', ('send', ['msg-2', 3])), inbox='messages')
+
+        msg_d = mock.get()
+        assert not msg_d.called
+
+        x.deliver(message=('node-3', ('init', [3])), inbox='messages')
+
+        sender, msg = deferred_result(msg_d)
+        assert msg == 'msg-2'
+
+        x.deliver(message=('node-2', ('send', ['msg-3', 3])), inbox='messages')
+        sender, msg = deferred_result(mock.get())
+        assert msg == 'msg-3'
+
+        x.deliver(message=('node-3', ('uninit', [])), inbox='messages')
+        x.deliver(message=('node-2', ('send', ['whatev', 3])), inbox='messages')
+        msg_d = mock.get()
+        assert not msg_d.called
+
+        with assert_raises(RoutingException):
+            x.deliver(message=(3, ('uninit', [])), inbox='messages')
+
+    def test_message_timeout(self):
+        self._create_relay(use_clock=True, max_message_age=10)
+
+        self.relay.deliver(message=('node-1', ('send', ['msg-1', 2])), inbox='messages')
+        self.clock.advance(11)
+        self.relay.deliver(message=('node-2', ('init', [2])), inbox='messages')
+        msg_d = self.mock.get()
+        assert not msg_d.called, "messages younger than max_message_age are delivered"
+
+        self.relay.deliver(message=('node-1', ('send', ['msg-2', 3])), inbox='messages')
+        self.clock.advance(9)
+        self.relay.deliver(message=('node-3', ('init', [3])), inbox='messages')
+        assert msg_d.called, "messages younger than max_message_age are delivered"
+
+
+if __name__ == '__main__':
+    unittest.main()
