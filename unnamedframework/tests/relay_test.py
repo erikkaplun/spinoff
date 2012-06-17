bbe6c77c0a0dc432b3095ebf03dcb721e05c2df2
Erik Allik <erik.allik@skype.net>
2012-06-17 23:36:19 +0300

    Cleaned up the imports in relay_test

--- unnamedframework/tests/relay_test.py
+++ unnamedframework/tests/relay_test.py
@@ -1,13 +1,8 @@
-from twisted.internet import reactor
 from twisted.internet.task import Clock
 from twisted.trial import unittest
 
-from unnamedframework.actor import IActor, RoutingException, Actor, InterfaceException
-from unnamedframework.util.testing import assert_raises, assert_not_raises, deferred_result
-
 from unnamedframework.actor.device.relay import Relay
-from unnamedframework.util.testing import MockActor
-from unnamedframework.util.testing import run, RootActor
+from unnamedframework.util.testing import MockActor, RootActor
 
 
 class HttpGatewayTest(unittest.TestCase):
