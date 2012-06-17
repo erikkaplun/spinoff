d50d2e67543cd56f5f1266a17d0807acd1ce5e60
Erik Allik <erik.allik@skype.net>
2012-06-17 13:05:09 +0300

    Split Actor into Actor and BaseActor; completely overhauled the Actor API; updated Relay, ZeroMQ and other actors to the changes

--- unnamedframework/actor/device/relay.py
+++ unnamedframework/actor/device/relay.py
@@ -1,43 +1,19 @@
 from twisted.internet import reactor
-from twisted.internet.defer import inlineCallbacks
 
-from unnamedframework.actor import Actor, RoutingException, InterfaceException
-from unnamedframework.util.async import sleep, CancelledError
+from unnamedframework.actor import BaseActor, RoutingException, InterfaceException
 
 
-class Relay(Actor):
+class Relay(BaseActor):
 
     def __init__(self, max_message_age=None, reactor=reactor, cleanup_interval=60, *args, **kwargs):
         super(Relay, self).__init__(*args, **kwargs)
         self._nodes = {}
         self._node_addrs = {}
-        self._pending = {}
+        self._pending_messages = {}
         self._max_message_age = max_message_age
         self._reactor = reactor
-        self._cleanup_interval = (cleanup_interval if max_message_age is None
-                                  else min(max_message_age, cleanup_interval))
-        self._stopped = False
 
-    @inlineCallbacks
-    def start(self):
-        while True:
-            currtime = self._reactor.seconds()
-            tmp = {}
-            for participant_id, pending in self._pending.items():
-                pending = [(m, c) for (m, c) in pending if 0 and c > currtime - self._max_message_age]
-                if pending:
-                    tmp[participant_id] = pending
-            self._pending = tmp
-            self._cleaning_d = sleep(self._cleanup_interval, reactor=self._reactor)
-            try:
-                yield self._cleaning_d
-            except CancelledError:
-                break
-
-    def stop(self):
-        self._cleaning_d.cancel()
-
-    def send(self, message):
+    def handle(self, message):
         if not isinstance(message, tuple) and len(message) >= 2:
             raise RoutingException("Messages to Relay should be tuples whose first element is the sender ID")
 
@@ -56,12 +32,12 @@ class Relay(Actor):
             self._nodes[sender_id] = participant_id
             self._node_addrs[participant_id] = sender_id
 
-            if participant_id in self._pending:
+            if participant_id in self._pending_messages:
                 currtime = self._reactor.seconds()
-                for message, created_at in self._pending[participant_id]:
+                for message, created_at in self._pending_messages[participant_id]:
                     if self._max_message_age is None or created_at > currtime - self._max_message_age:
                         self.put(message=(sender_id, message))
-                del self._pending[participant_id]
+                del self._pending_messages[participant_id]
         elif command == 'uninit':
             if sender_id not in self._nodes:
                 raise RoutingException("Node not registered")
@@ -74,6 +50,14 @@ class Relay(Actor):
             if recipient in self._node_addrs:
                 self.put(message=(self._node_addrs[recipient], payload))
             else:
-                self._pending.setdefault(recipient, []).append((payload, self._reactor.seconds()))
+                self._pending_messages.setdefault(recipient, []).append((payload, self._reactor.seconds()))
         else:
             raise InterfaceException("No such command: %s" % command)
+
+        currtime = self._reactor.seconds()
+        tmp = {}
+        for participant_id, pending in self._pending_messages.items():
+            pending = [(m, c) for (m, c) in pending if self._max_message_age is None or c > currtime - self._max_message_age]
+            if pending:
+                tmp[participant_id] = pending
+        self._pending_messages = tmp
