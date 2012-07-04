40b57752a6890da8e5cf872c04cad2c50975e53c
Erik Allik <erik.allik@skype.net>
2012-07-04 17:50:14 +0200

    Made relay_test use Container instead of RootActor

--- unnamedframework/tests/relay_test.py
+++ unnamedframework/tests/relay_test.py
@@ -2,7 +2,7 @@ from twisted.internet.task import Clock
 from twisted.trial import unittest
 
 from unnamedframework.actor.device.relay import Relay
-from unnamedframework.util.testing import MockActor, RootActor
+from unnamedframework.util.testing import MockActor, Container
 
 
 class HttpGatewayTest(unittest.TestCase):
@@ -13,7 +13,7 @@ class HttpGatewayTest(unittest.TestCase):
     def _create_relay(self, use_clock=False, **kwargs):
         self.clock = Clock()
 
-        self.root = RootActor.spawn()
+        self.root = Container.spawn()
         self.mock = self.root.spawn(MockActor)
         self.relay = self.root.spawn(Relay, self.mock, reactor=self.clock, **kwargs)
 
