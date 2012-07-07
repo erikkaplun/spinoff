11fecf2295b8ac3706cf354fe1b2c4662aa3871b
Erik Allik <erik.allik@skype.net>
2012-07-08 01:46:06 +0200

    Made zeromq_test run faster

--- spinoff/tests/zeromq_test.py
+++ spinoff/tests/zeromq_test.py
@@ -7,7 +7,7 @@ from spinoff.util.testing import assert_not_raises, contain
 
 
 _wait_msg = lambda d: with_timeout(4.0, d)
-_wait_slow_joiners = lambda n=1: sleep(0.05 * n)
+_wait_slow_joiners = lambda n=1: sleep(0.001 * n)  # try increasing this if tests fail
 
 
 ADDR = 'ipc://test'
