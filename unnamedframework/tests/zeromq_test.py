ae45474c6331022918b77a10c7d00b946ea66dd1
Erik Allik <erik.allik@skype.net>
2012-07-08 01:40:26 +0200

    Made zeromq_test pass again

--- unnamedframework/tests/zeromq_test.py
+++ unnamedframework/tests/zeromq_test.py
@@ -3,7 +3,7 @@ from twisted.trial import unittest
 
 from unnamedframework.actor.transport.zeromq import ZmqRouter, ZmqDealer
 from unnamedframework.util.async import TimeoutError, sleep, with_timeout
-from unnamedframework.util.testing import assert_not_raises, run
+from unnamedframework.util.testing import assert_not_raises, contain
 
 
 _wait_msg = lambda d: with_timeout(4.0, d)
@@ -13,44 +13,24 @@ _wait_slow_joiners = lambda n=1: sleep(0.05 * n)
 ADDR = 'ipc://test'
 
 
-class TestCaseBase(unittest.TestCase):
-
-    def setUp(self):
-        self._z_components = []
-
-    def _make(self, cls, endpoint, identity=None):
-        ret = run(cls(endpoint, identity))
-        self._z_components.append(ret[1])
-        return ret
-
-    def _make_dealer(self, *args, **kwargs):
-        return self._make(ZmqDealer, *args, **kwargs)
-
-    def _make_router(self, *args, **kwargs):
-        return self._make(ZmqRouter, *args, **kwargs)
-
-    def tearDown(self):
-        for component in self._z_components:
-            component.stop()
-
-
-class RouterDealerTestCase(TestCaseBase):
+class RouterDealerTestCase(unittest.TestCase):
 
     @inlineCallbacks
-    def _do_test_router_with_n_dealers(self, n):
-        root, router = self._make_router(ADDR)
-        dealers = []
-        for i in range(n):
-            root, dealer = self._make_dealer(ADDR, identity='dude%s' % i)
-            dealers.append((dealer, root))
-        yield _wait_slow_joiners(n)
+    def _do_test_router_with_n_dealers(self, n=1):
+        with contain(ZmqRouter(ADDR)) as (container, router):
+            dealers = []
+            for i in range(n):
+                dealer = container.spawn(ZmqDealer(ADDR, identity='dude%s' % i))
+                dealers.append(dealer)
 
-        for dealer, root in dealers:
-            msg = 'PING%s' % i
+            yield _wait_slow_joiners(n)
 
-            router.send(message=(dealer.identity, msg))
-            with assert_not_raises(TimeoutError, "should have received a message"):
-                assert msg == (yield root.wait())
+            for dealer in dealers:
+                msg = 'PING%s' % i
+
+                router.send(message=(dealer.identity, msg))
+                with assert_not_raises(TimeoutError, "should have received a message"):
+                    assert msg == (yield container.wait())
 
     def test_router_with_1_dealer(self):
         return self._do_test_router_with_n_dealers(1)
