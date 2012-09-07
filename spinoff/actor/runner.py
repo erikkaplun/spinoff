64621ccc230983ad5e32a93c6009ab9a92b0fe18
Erik Allik <erik.allik@skype.net>
2012-09-07 11:01:18 +0300

    startnode/ActorRunner: added support for specifying actor name and set up remoting; use the logging module instead of print()

--- spinoff/actor/runner.py
+++ spinoff/actor/runner.py
@@ -1,14 +1,17 @@
 from __future__ import print_function
 
-import sys
+import re
 
 from twisted.application.service import Service
 from twisted.internet import reactor
 from twisted.internet.error import ReactorNotRunning
-from twisted.python import log
 from twisted.python.failure import Failure
+from txzmq import ZmqFactory, ZmqRouterConnection
 
-from spinoff.actor import spawn, Actor, Props
+from spinoff.actor import spawn, Actor, Props, set_default_node, Node
+from spinoff.actor._actor import _validate_nodeid, _VALID_IP_RE
+from spinoff.actor.remoting import Hub, HubWithNoRemoting
+from spinoff.util.logging import log, err, panic
 from spinoff.util.pattern_matching import ANY
 
 
@@ -17,24 +20,47 @@ _EMPTY = object()
 
 class ActorRunner(Service):
 
-    def __init__(self, actor_cls, init_params={}, initial_message=_EMPTY):
+    def __init__(self, actor_cls, init_params={}, initial_message=_EMPTY, nodeid=None, name=None):
+        if nodeid:
+            _validate_nodeid(nodeid)
         self._init_params = init_params
         self._initial_message = initial_message
         self._actor_cls = actor_cls
         self._wrapper = None
+        self._nodeid = nodeid
+        self._name = name
 
     def startService(self):
         actor_path = self._actor_path = '%s.%s' % (self._actor_cls.__module__, self._actor_cls.__name__)
 
-        log.msg("*** Running: %s" % (actor_path,))
+        log("Running: %s" % (actor_path,), "@ /%s" % (self._name,) if self._name else '')
 
         def start_actor():
+            if self._nodeid:
+                if not re.match(_VALID_IP_RE, self._nodeid):
+                    err("Node ID to IP resolution not supported yet; "
+                        "provide an <ip-addr:port> parameter to set up remoting")
+                    reactor.stop()
+                    return
+
+                log("Setting up remoting; node ID =", self._nodeid)
+                ip_addr = self._nodeid
+                f1 = ZmqFactory()
+                insock = ZmqRouterConnection(f1, identity='tcp://%s' % (ip_addr,))
+                outsock = ZmqRouterConnection(f1, identity='tcp://%s' % (ip_addr,))
+                hub = Hub(insock, outsock, node=self._nodeid)
+            else:
+                log("No remoting requested; specify `--remoting/-r <nodeid>` (nodeid=host:port) to set up remoting")
+                hub = HubWithNoRemoting()
+
+            set_default_node(Node(hub=hub))
+
             try:
-                self._wrapper = spawn(Props(Wrapper, Props(self._actor_cls, **self._init_params)), name='_runner')
+                self._wrapper = spawn(Props(Wrapper, Props(self._actor_cls, **self._init_params), spawn_at=self._name), name='_runner')
             except Exception:
-                print("*** Failed to start wrapper for %s\n" % (actor_path,), file=sys.stderr)
-                Failure().printTraceback(file=sys.stderr)
-                return
+                panic("Failed to start wrapper for %s\n" % (actor_path,),
+                      Failure().getTraceback())
+                reactor.stop()
             else:
                 if self._initial_message is not _EMPTY:
                     self._wrapper << ('_forward', self._initial_message)
@@ -51,25 +77,30 @@ class ActorRunner(Service):
 
 
 class Wrapper(Actor):
-    def __init__(self, actor_factory):
+    def __init__(self, actor_factory, spawn_at):
         self.actor_factory = actor_factory
+        self.spawn_at = spawn_at
 
     def pre_start(self):
-        name = (self.actor_factory.__name__
+        name = (self.spawn_at
+                if self.spawn_at else
+                self.actor_factory.__name__
                 if isinstance(self.actor_factory, type) else
                 self.actor_factory.cls.__name__)
         self.actor = self.watch(self.node.spawn(self.actor_factory, name=name))
+        log("Spawned %s" % (self.actor,))
 
     def receive(self, message):
         if message == ('_forward', ANY):
             _, payload = message
             self.actor << payload
         elif message == ('terminated', ANY):
+            log("Actor terminated, stopping...")
             _, actor = message
             assert actor == self.actor
             self.stop()
         else:
-            print("*** Actor sent message to parent: %r" % (message,), file=sys.stderr)
+            log("Actor sent message to parent: %r" % (message,))
 
     def post_stop(self):
         try:
--- twisted/plugins/startnode.py
+++ twisted/plugins/startnode.py
@@ -7,7 +7,9 @@ from twisted.plugin import IPlugin
 from twisted.python import usage
 from zope.interface import implements
 
+from spinoff.actor._actor import _validate_nodeid
 from spinoff.actor.runner import ActorRunner
+from spinoff.util.logging import fatal
 
 
 class _EMPTY(object):
@@ -22,11 +24,13 @@ _EMPTY = _EMPTY()
 class Options(usage.Options):
 
     optParameters = [
-        ['actor', 'a', None, "The actor to spawn."],
-        ['params', 'p', _EMPTY, (u"Parameters to initialize the actor with.\n\n"
+        ['actor', 'a', None, "The [a]ctor to spawn."],
+        ['params', 'i', _EMPTY, (u"Parameters to [i]nitialize the actor with.\n\n"
                                  u"Parsed as `dict(<params>)` and passed as **kwargs.\n\n"
                                  u"\b")],  # so that Twisted wouldn't strip off the line endings before [default: <empty>]
-        ['message', 'm', _EMPTY, "Message to send to the actor"],
+        ['message', 'm', _EMPTY, "[m]essage to send to the actor"],
+        ['remoting', 'r', None, "Set up [r]emoting with"],
+        ['name', 'n', None, "Set the [n]ame of the actor"],
     ]
 
 
@@ -40,26 +44,25 @@ class ActorRunnerMaker(object):
     def makeService(self, options):
         actor = options['actor']
         if not actor:
-            print("error: no actor specified", file=sys.stderr)
+            fatal("error: no actor specified")
             sys.exit(1)
 
         try:
             module_path, actor_cls_name = actor.rsplit('.', 1)
         except ValueError:
-            print("error: invalid path to actor %s" % actor, file=sys.stderr)
+            fatal("error: invalid path to actor %s" % actor)
             sys.exit(1)
 
         try:
             mod = __import__(module_path, globals(), locals(), [actor_cls_name], -1)
         except ImportError:
-            print("error: could not import %s" % actor, file=sys.stderr)
-            # failure.Failure().printTraceback(file=sys.stderr)
+            fatal("error: could not import %s" % actor)
             sys.exit(1)
 
         try:
             actor_cls = getattr(mod, actor_cls_name)
         except AttributeError:
-            print("error: no such actor %s" % actor, file=sys.stderr)
+            fatal("error: no such actor %s" % actor)
             sys.exit(1)
 
         kwargs = {}
@@ -69,7 +72,7 @@ class ActorRunnerMaker(object):
             try:
                 params = eval(params)
             except:
-                print("error: could not parse parameters", file=sys.stderr)
+                fatal("error: could not parse parameters")
                 sys.exit(1)
             else:
                 kwargs['init_params'] = params
@@ -79,12 +82,33 @@ class ActorRunnerMaker(object):
             try:
                 initial_message = eval(initial_message)
             except:
-                print("error: could not parse initial message", file=sys.stderr)
+                fatal("error: could not parse initial message")
                 sys.exit(1)
             else:
-                kwargs = {'initial_message': initial_message}
+                kwargs['initial_message'] = initial_message
+
+        if options['name']:
+            name = options['name']
+            if '/' in name:
+                fatal("invalid name: names cannot contain slashes")
+                sys.exit(1)
+            else:
+                kwargs['name'] = name
+
+        if options['remoting']:
+            nodeid = options['remoting']
+            try:
+                _validate_nodeid(nodeid)
+            except TypeError:
+                fatal("invalid node ID")
+                sys.exit(1)
+            else:
+                kwargs['nodeid'] = nodeid
 
         return ActorRunner(actor_cls, **kwargs)
+
+    def __repr__(self):
+        return '<#bootstrap#>'
 
 
 serviceMaker = ActorRunnerMaker()
