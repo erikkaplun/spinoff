e66fbec38dbc1aec36cfcdd0965e74a2ac815418
Erik Allik <erik.allik@skype.net>
2012-09-10 15:40:05 +0300

    Added --supervise, --keeprunning, and --remotedebugging* flags/support to the startnode command

--- twisted/plugins/startnode.py
+++ twisted/plugins/startnode.py
@@ -2,14 +2,21 @@ from __future__ import print_function
 
 import sys
 
-from twisted.application.service import IServiceMaker
+from twisted.application.service import IServiceMaker, MultiService
+from twisted.application.internet import TCPServer
+from twisted.cred.portal import Portal
+from twisted.cred.checkers import InMemoryUsernamePasswordDatabaseDontUse
 from twisted.plugin import IPlugin
 from twisted.python import usage
+from twisted.conch.manhole import ColoredManhole
+from twisted.conch.manhole_ssh import ConchFactory, TerminalRealm
+from twisted.conch.insults.insults import ServerProtocol
 from zope.interface import implements
+
 
 from spinoff.actor._actor import _validate_nodeid
 from spinoff.actor.runner import ActorRunner
-from spinoff.util.logging import fatal
+from spinoff.util.logging import fatal, log
 
 
 class _EMPTY(object):
@@ -31,6 +38,15 @@ class Options(usage.Options):
         ['message', 'm', _EMPTY, "[m]essage to send to the actor"],
         ['remoting', 'r', None, "Set up [r]emoting with"],
         ['name', 'n', None, "Set the [n]ame of the actor"],
+        ['supervise', 's', 'stop', "Set how the spawned actor is [s]upervised in case of failures"],
+
+        ['remotedebuggingport', 'p', 6022, "[p]rt on which to start the SSH remote debug console server"],
+        ['remotedebuggingusername', 'u', 'debug', "[u]sername to log on to the SSH remote debug console"],
+        ['remotedebuggingpassword', 'P', '', "[P]assword to log on to the SSH remote debug console"],
+    ]
+    optFlags = [
+        ['keeprunning', 'k', "Whether the actor should be re-spawned on termination"],
+        ['remotedebugging', 'd', "Whether to start a SSH remote [d]ebug console server"],
     ]
 
 
@@ -105,7 +121,44 @@ class ActorRunnerMaker(object):
             else:
                 kwargs['nodeid'] = nodeid
 
-        return ActorRunner(actor_cls, **kwargs)
+        if options['supervise']:
+            supervise_option = options['supervise']
+            if supervise_option not in ('stop', 'restart', 'resume'):
+                fatal("Supervision should be either 'stop', 'restart' or 'resume'")
+                sys.exit(1)
+            kwargs['supervise'] = supervise_option
+
+        kwargs['keep_running'] = options['keeprunning']
+
+        m = MultiService()
+        actor_runner = ActorRunner(actor_cls, **kwargs)
+        actor_runner.setServiceParent(m)
+
+        # manhole
+
+        if options['remotedebugging']:
+            port = options['remotedebuggingport']
+            try:
+                port = int(port)
+            except ValueError:
+                fatal("Invalid port specified: %r" % port)
+                sys.exit(1)
+
+            username = options['remotedebuggingusername']
+            password = options['remotedebuggingpassword']
+
+            manhole = self.make_manhole_server(port, username, password)
+            manhole.setServiceParent(m)
+
+        return m
+
+    def make_manhole_server(self, port, username, password):
+        rlm = TerminalRealm()
+        rlm.chainedProtocolFactory = lambda: ServerProtocol(ColoredManhole, None)
+
+        auth_checker = InMemoryUsernamePasswordDatabaseDontUse(**{username: password})
+
+        return TCPServer(port, ConchFactory(Portal(rlm, [auth_checker])))
 
     def __repr__(self):
         return '<#bootstrap#>'
