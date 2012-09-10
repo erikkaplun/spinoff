002127fc113673148c9e0075735d24568e0da72a
Erik Allik <erik.allik@skype.net>
2012-09-10 14:00:10 +0300

    Added supervise and keep_running params/support to ActorRunner and made it use Actor.using instead of Props

--- spinoff/actor/runner.py
+++ spinoff/actor/runner.py
@@ -8,19 +8,20 @@ from twisted.internet.error import ReactorNotRunning
 from twisted.python.failure import Failure
 from txzmq import ZmqFactory, ZmqRouterConnection
 
-from spinoff.actor import spawn, Actor, Props, set_default_node, Node
+from spinoff.actor import spawn, Actor, set_default_node, Node
 from spinoff.actor._actor import _validate_nodeid, _VALID_IP_RE
 from spinoff.actor.remoting import Hub, HubWithNoRemoting
 from spinoff.util.logging import log, err, panic
 from spinoff.util.pattern_matching import ANY
 
+from spinoff.actor.supervision import Stop, Restart, Resume
 
 _EMPTY = object()
 
 
 class ActorRunner(Service):
 
-    def __init__(self, actor_cls, init_params={}, initial_message=_EMPTY, nodeid=None, name=None):
+    def __init__(self, actor_cls, init_params={}, initial_message=_EMPTY, nodeid=None, name=None, supervise='stop', keep_running=False):
         if nodeid:
             _validate_nodeid(nodeid)
         self._init_params = init_params
@@ -29,6 +30,8 @@ class ActorRunner(Service):
         self._wrapper = None
         self._nodeid = nodeid
         self._name = name
+        self._supervise = supervise
+        self._keep_running = keep_running
 
     def startService(self):
         actor_path = self._actor_path = '%s.%s' % (self._actor_cls.__module__, self._actor_cls.__name__)
@@ -53,10 +56,12 @@ class ActorRunner(Service):
                 log("No remoting requested; specify `--remoting/-r <nodeid>` (nodeid=host:port) to set up remoting")
                 hub = HubWithNoRemoting()
 
-            set_default_node(Node(hub=hub))
+            supervision = {'stop': Stop, 'restart': Restart, 'resume': Resume}[self._supervise]
+            set_default_node(Node(hub=hub, root_supervision=supervision))
 
             try:
-                self._wrapper = spawn(Props(Wrapper, Props(self._actor_cls, **self._init_params), spawn_at=self._name), name='_runner')
+                self._wrapper = spawn(Wrapper.using(self._actor_cls.using(**self._init_params),
+                                                    spawn_at=self._name, keep_running=self._keep_running), name='_runner')
             except Exception:
                 panic("Failed to start wrapper for %s\n" % (actor_path,),
                       Failure().getTraceback())
@@ -77,28 +82,28 @@ class ActorRunner(Service):
 
 
 class Wrapper(Actor):
-    def __init__(self, actor_factory, spawn_at):
+    def __init__(self, actor_factory, spawn_at, keep_running):
         self.actor_factory = actor_factory
         self.spawn_at = spawn_at
+        self.keep_running = keep_running
+
+    def _do_spawn(self):
+        self.actor = self.watch(self.node.spawn(self.actor_factory, name=self.spawn_at))
+        Events.log(Message("Spawned %s" % (self.actor,)))
 
     def pre_start(self):
-        name = (self.spawn_at
-                if self.spawn_at else
-                self.actor_factory.__name__
-                if isinstance(self.actor_factory, type) else
-                self.actor_factory.cls.__name__)
-        self.actor = self.watch(self.node.spawn(self.actor_factory, name=name))
-        log("Spawned %s" % (self.actor,))
+        self._do_spawn()
 
     def receive(self, message):
         if message == ('_forward', ANY):
             _, payload = message
             self.actor << payload
-        elif message == ('terminated', ANY):
-            log("Actor terminated, stopping...")
+        elif message == ('terminated', self.actor):
             _, actor = message
-            assert actor == self.actor
-            self.stop()
+            if self.keep_running:
+                self._do_spawn()
+            else:
+                self.stop()
         else:
             log("Actor sent message to parent: %r" % (message,))
 
