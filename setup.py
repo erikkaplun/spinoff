049fd99929ce39af93451a7b57c16e304997a1b5
Erik Allik <eallik@gmail.com>
2012-11-21 19:01:02 +0200

    Version 0.5.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name="spinoff",
     description="Framework for writing distributed, fault tolerant and scalable applications",
     long_description=long_description,
-    version="0.5.2",
+    version="0.5.3",
     packages=find_packages(),
 
     install_requires=[
