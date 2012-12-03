1d6dbd2891236e70badf0c5c63c9b97c0724ed5f
Erik Allik <eallik@gmail.com>
2012-12-03 17:40:30 +0200

    Bumped version to 0.5.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name="spinoff",
     description="Framework for writing distributed, fault tolerant and scalable applications",
     long_description=long_description,
-    version="0.5.4",
+    version="0.5.5",
     packages=find_packages(),
 
     install_requires=[
