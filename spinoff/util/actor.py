b29a46c3afa94b22fa1eb73830939936b1aaff0d
Erik Allik <eallik@gmail.com>
2012-12-07 17:03:07 +0100

    Refactored s.u.a.wrap_globals in a way that avoids yields in finally-blocks

--- spinoff/util/actor.py
+++ spinoff/util/actor.py
@@ -11,9 +11,8 @@ from twisted.internet.defer import inlineCallbacks, DebugInfo, setDebugging
 from spinoff.actor import Actor, Node
 from spinoff.actor.events import Events
 from spinoff.actor.remoting import HubWithNoRemoting
-from spinoff.util.async import _process_idle_calls, _idle_calls
+from spinoff.util.async import _process_idle_calls, deferred_with
 from spinoff.util.testing import ErrorCollector, Unclean
-from spinoff.util.logging import dbg
 
 
 def TestNode():
@@ -29,7 +28,6 @@ def wrap_globals(globals):
 
         @functools.wraps(fn)
         @deferred(timeout=fn.timeout if hasattr(fn, 'timeout') else 1.0)
-        @inlineCallbacks
         def ret():
             # dbg("\n============================================\n")
 
@@ -42,56 +40,45 @@ def wrap_globals(globals):
             # having each Node have its own Events instance.
             Events.reset()
 
-            with ErrorCollector():
-                # setDebugging(1)
-                try:
-                    yield fn()
-                finally:
-                    # dbg("TESTWRAP: ------------------------- cleaning up after %s" % (fn.__name__,))
+            def check_memleaks():
+                if '__pypy__' not in sys.builtin_module_names:
+                    gc.collect()
+                    for trash in gc.garbage[:]:
+                        if isinstance(trash, DebugInfo):
+                            # dbg("DEBUGINFO: __del__")
+                            if trash.failResult is not None:
+                                exc = Unclean(repr(trash.failResult.value) + '\n' + str(trash._getDebugTracebacks()))
+                                trash.__dict__.clear()
+                                raise exc
+                            gc.garbage.remove(trash)
 
-                    # yield Node.stop_all()
+                    assert not gc.garbage, "Memory leak detected: %r" % (gc.garbage,)
 
-                    if _idle_calls:
-                        # dbg("TESTWRAP: processing all remaining scheduled calls...")
-                        _process_idle_calls()
-                        # dbg("TESTWRAP: ...scheduled calls done.")
+                    # if gc.garbage:
+                    #     dbg("GARGABE: detected after %s:" % (fn.__name__,), len(gc.garbage))
+                    #     import objgraph as ob
+                    #     import os
 
-                    if '__pypy__' not in sys.builtin_module_names:
-                        gc.collect()
-                        for trash in gc.garbage[:]:
-                            if isinstance(trash, DebugInfo):
-                                # dbg("DEBUGINFO: __del__")
-                                if trash.failResult is not None:
-                                    exc = Unclean(repr(trash.failResult.value) + '\n' + str(trash._getDebugTracebacks()))
-                                    trash.__dict__.clear()
-                                    raise exc
-                                gc.garbage.remove(trash)
+                    #     def dump_chain(g_):
+                    #         def calling_test(x):
+                    #             if not isframework(x):
+                    #                 return None
+                    #         import spinoff
+                    #         isframework = lambda x: type(x).__module__.startswith(spinoff.__name__)
+                    #         ob.show_backrefs([g_], filename='backrefs.png', max_depth=100, highlight=isframework)
 
-                        assert not gc.garbage, "Memory leak detected"
+                    #     for gen in gc.garbage:
+                    #         dump_chain(gen)
+                    #         dbg("   TESTWRAP: mem-debuggin", gen)
+                    #         import pdb; pdb.set_trace()
+                    #         os.remove('backrefs.png')
 
-                        # if gc.garbage:
-                        #     dbg("GARGABE: detected after %s:" % (fn.__name__,), len(gc.garbage))
-                        #     import objgraph as ob
-                        #     import os
-
-                        #     def dump_chain(g_):
-                        #         def calling_test(x):
-                        #             if not isframework(x):
-                        #                 return None
-                        #         import spinoff
-                        #         isframework = lambda x: type(x).__module__.startswith(spinoff.__name__)
-                        #         ob.show_backrefs([g_], filename='backrefs.png', max_depth=100, highlight=isframework)
-
-                        #     for gen in gc.garbage:
-                        #         dump_chain(gen)
-                        #         dbg("   TESTWRAP: mem-debuggin", gen)
-                        #         import pdb; pdb.set_trace()
-                        #         os.remove('backrefs.png')
-
-                        # to avoid the above assertion from being included as part of any tracebacks from the test
-                        # function--the last line of a context managed block is included in tracebacks originating
-                        # from context managers.
-                        pass
+            return (
+                deferred_with(ErrorCollector(), fn)
+                .addBoth(lambda result: Node.stop_all().addCallback(lambda _: result))
+                .addBoth(lambda result: (_process_idle_calls(), result)[-1])
+                .addBoth(lambda result: (check_memleaks(), result)[-1])
+            )
 
         return ret
 
