828cb0b8d0aeb001d34a9841284374a984933dfd
Erik Allik <eallik@gmail.com>
2013-02-13 12:34:24 +0100

    Refactoring the remoting

--- spinoff/actor/remoting/hub.py
+++ spinoff/actor/remoting/hub.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import time
+import struct
 import cPickle as pickle
 from types import GeneratorType
 
@@ -9,11 +10,15 @@ from gevent import sleep, spawn
 from gevent.socket import gethostbyname
 
 from spinoff.actor.events import Events, DeadLetter
-from spinoff.actor.remoting.hublogic import HubLogic, Connect, Disconnect, SendPacket, Deliver, NotifySendFailed, NotifyNodeDown
+from spinoff.actor.remoting.hublogic import HubLogic, Connect, Disconnect, SigDisconnect, Send, Ping, Receive, SendFailed, NodeDown, IN, OUT
 from spinoff.util.logging import dbg
 
 
 __all__ = ['Hub']
+
+
+MSG_HEADER_FORMAT = '!I'
+DISCONNECT_SIGNAL = '\0\0\0\0'
 
 
 class Hub(object):
@@ -30,87 +35,119 @@ class Hub(object):
 
     HEARTBEAT_MAX_SILENCE = 3.0
 
-    def __init__(self, node_id, node_down_fn=lambda ref, node_id: ref << ('_node_down', node_id),
-                 deliver_fn=lambda msg_bytes, sender_node_id: print("deliver", msg_bytes, "from", sender_node_id)):
-        self._node_down_fn = node_down_fn
-        self._deliver_fn = deliver_fn
+    def __init__(self, node_id, on_node_down=lambda ref, node_id: ref << ('_node_down', node_id),
+                 on_receive=lambda msg_bytes, sender_node_id: print("deliver", msg_bytes, "from", sender_node_id)):
+        self._on_node_down = on_node_down
+        self._on_receive = on_receive
         self._logic = HubLogic(heartbeat_interval=self.HEARTBEAT_INTERVAL,
                                heartbeat_max_silence=self.HEARTBEAT_MAX_SILENCE)
         self._ctx = zmq.Context()
         self._ctx.linger = 0
-        self._sock = self._ctx.socket(zmq.ROUTER)
-        self._sock.identity = node_id
-        self._sock.bind(node_id_to_zmq_endpoint(node_id))
-        self._listener = spawn(self._listen)
-        self._hearbeater = spawn(self._send_recv_heartbeat)
+        self._insock = self._ctx.socket(zmq.ROUTER)
+        self._outsock = self._ctx.socket(zmq.ROUTER)
+        self._insock.identity = self._outsock.identity = node_id
+        self._insock.bind(node_id_to_zmq_endpoint(node_id))
+        self._listener_in = spawn(self._listen, self._insock, IN)
+        self._listener_out = spawn(self._listen, self._outsock, OUT)
+        self._heartbeater = spawn(self._send_recv_heartbeat)
         self._watched_nodes = {}
+        self._initialized = True
 
-    def _listen(self):
-        recv, message_received, execute, t = self._sock.recv_multipart, self._logic.message_received, self._execute, time.time
+    def _listen(self, sock, dir):
+        recv, t, execute, message_received, ping_received, sig_disconnect_received = (
+            sock.recv_multipart, time.time, self._execute, self._logic.message_received, self._logic.ping_received, self._logic.sig_disconnect_received)
         while True:
             sender_node_id, msg_bytes = recv()
-            execute(message_received(sender_node_id, msg_bytes, t()))
+            if msg_bytes == DISCONNECT_SIGNAL:
+                execute(sig_disconnect_received, sender_node_id)
+            else:
+                version, msg_body_bytes = struct.unpack(MSG_HEADER_FORMAT, msg_bytes[:4]), msg_bytes[4:]
+                if msg_body_bytes:
+                    execute(message_received, dir, sender_node_id, version, msg_body_bytes, t())
+                else:
+                    execute(ping_received, dir, sender_node_id, version, t())
 
     def _send_recv_heartbeat(self):
         interval, heartbeat, execute, t = self.HEARTBEAT_INTERVAL, self._logic.heartbeat, self._execute, time.time
         while True:
-            execute(heartbeat(t()))
+            execute(heartbeat, t())
             sleep(interval)
 
     def send_message(self, node_id, msg_handle):
-        self._execute(self._logic.send_message(node_id, msg_handle, time.time()))
+        self._execute(self._logic.send_message, node_id, msg_handle, time.time())
 
-    def watch_node(self, node_id, watcher):
+    def watch_node(self, node_id, watch_handle):
         if node_id not in self._watched_nodes:
-            self._watched_nodes[node_id] = set([watcher])
-            self._execute(self._logic.watch_new_node(node_id, time.time()))
+            self._watched_nodes[node_id] = set([watch_handle])
+            self._execute(self._logic.ensure_connected, node_id, time.time())
         else:
-            self._watched_nodes[node_id].add(watcher)
+            self._watched_nodes[node_id].add(watch_handle)
 
-    def unwatch_node(self, node_id, watcher):
+    def unwatch_node(self, node_id, watch_handle):
         try:
-            self._watched_nodes[node_id].discard(watcher)
+            self._watched_nodes[node_id].discard(watch_handle)
         except IndexError:
             pass
 
     def stop(self):
-        self._listener.kill()
-        self._execute(self._logic.shutdown())
-        sleep(.01)  # XXX: needed?
-        self._sock.shutdown()
-        self._ctx.shutdown()
+        if hasattr(self, '_heartbeater'):
+            self._heartbeater.kill()
+            del self._heartbeater
+        if hasattr(self, '_listener_out'):
+            self._listener_out.kill()
+            del self._listener_out
+        if hasattr(self, '_listener_in'):
+            self._listener_in.kill()
+            del self._listener_in
+        if hasattr(self, '_initialized'):
+            self._execute(self._logic.shutdown)
+            del self._logic, self._initialized
+            sleep(.01)  # XXX: needed?
+        if hasattr(self, '_ctx'):
+            self._ctx.destroy(linger=0)
+            del self._ctx, self._insock
 
-    def _execute(self, actions):
-        for action in flatten(actions):
+    def _execute(self, fn, *args, **kwargs):
+        for action in flatten(fn(*args, **kwargs)):
             cmd = action[0]
-            dbg("%s: %s" % (cmd, ", ".join(repr(x) for x in action[1:])))
-            if cmd is SendPacket:
-                _, node_id, data = action
-                self._sock.send_multipart([node_id, data])
+            dbg("%s -> %s: %s" % (fn.__name__, cmd, ", ".join(repr(x) for x in action[1:])))
+            if cmd is Send:
+                _, dir, node_id, version, msg_handle = action
+                (self._outsock if dir == OUT else self._insock).send_multipart([node_id, struct.pack(MSG_HEADER_FORMAT, version) + msg_handle.serialise()])
+            elif cmd is Ping:
+                _, dir, node_id, version = action
+                (self._outsock if dir == OUT else self._insock).send_multipart([node_id, struct.pack(MSG_HEADER_FORMAT, version)])
+            elif cmd is SigDisconnect:
+                _, dir, node_id = action
+                (self._outsock if dir == OUT else self._insock).send_multipart([node_id, DISCONNECT_SIGNAL])
             elif cmd is Connect:
                 _, node_id = action
-                self._sock.connect(node_id_to_zmq_endpoint(node_id))
+                self._outsock.connect(node_id_to_zmq_endpoint(node_id))
+                sleep(.01)
             elif cmd is Disconnect:
                 _, node_id = action
-                try:
-                    self._sock.disconnect(node_id_to_zmq_endpoint(node_id))
-                except zmq.ZMQError:
-                    pass
-            elif cmd is Deliver:
+                self._outsock.disconnect(node_id_to_zmq_endpoint(node_id))
+            elif cmd is SigDisconnect:
+                _, dir, node_id = action
+                (self._outsock if dir == OUT else self._insock).send_multipart([node_id, DISCONNECT_SIGNAL])
+            elif cmd is Receive:
                 _, msg_bytes, sender_node_id = action
-                self._deliver_fn(msg_bytes, sender_node_id)
-            elif cmd is NotifySendFailed:
+                self._on_receive(msg_bytes, sender_node_id)
+            elif cmd is SendFailed:
                 _, msg_handle = action
                 msg_handle.send_failed()
-            elif cmd is NotifyNodeDown:
+            elif cmd is NodeDown:
                 _, node_id = action
-                for watcher in self._watched_nodes.pop(node_id, []):
-                    self._node_down_fn(watcher, node_id)
+                for watch_handle in self._watched_nodes.pop(node_id, []):
+                    self._on_node_down(watch_handle, node_id)
             else:
                 assert False, "unknown command: %r" % (cmd,)
 
+    def __del__(self):
+        self.stop()
+
     def __repr__(self):
-        return "Hub(%s)" % (self._sock.identity,)
+        return "Hub(%s)" % (self._insock.identity,)
 
 
 def node_id_to_zmq_endpoint(node_id):
--- spinoff/actor/remoting/hublogic.py
+++ spinoff/actor/remoting/hublogic.py
@@ -1,27 +1,22 @@
 from __future__ import print_function
 
-import struct
+from itertools import chain
 
 from spinoff.util.python import enumrange
 from spinoff.util.logging import dbg
 
 
-DISCONNECT, MESSAGE = 1, 2
-MSG_HEADER_FORMAT = '!I'
-NO_CHANGE = object()
-
-DISCONNECT_SIGNAL = '\0\0\0\0'
-
-
 # signals from HubLogic to Hub or test code
-Connect, Disconnect, SendPacket, Deliver, NotifySendFailed, NotifyNodeDown = enumrange('Connect', 'Disconnect', 'SendPacket', 'Deliver', 'NotifySendFailed', 'NotifyNodeDown')
+Connect, Disconnect, SigDisconnect, Send, Ping, Receive, SendFailed, NodeDown = enumrange('Connect', 'Disconnect', 'SigDisconnect', 'Send', 'Ping', 'Receive', 'SendFailed', 'NodeDown')
+IN, OUT = enumrange('IN', 'OUT')
 
 
 class HubLogic(object):
     def __init__(self, heartbeat_interval, heartbeat_max_silence):
         self.heartbeat_interval = heartbeat_interval
         self.heartbeat_max_silence = heartbeat_max_silence
-        self.channels = set()
+        self.channels_in = set()
+        self.channels_out = set()
         self.last_seen = {}
         self.last_sent = {}
         self.versions = {}
@@ -30,95 +25,102 @@ class HubLogic(object):
         # beginning of a pack-*et could be identified as a DISCONNECT
         self.version = 0
 
+    def send_message(self, recipient_node_id, msg_handle, current_time):
+        # is it a new connection, or an existing but not yet active connection?
+        if recipient_node_id in self.channels_in:
+            yield Send, IN, recipient_node_id, self._next_version(), msg_handle
+        elif recipient_node_id in self.channels_out:
+            if recipient_node_id not in self.queues:
+                yield Send, OUT, recipient_node_id, self._next_version(), msg_handle
+            else:
+                self.queues[recipient_node_id].append(msg_handle)
+        else:
+            yield Connect, recipient_node_id
+            if self._needs_ping(recipient_node_id, current_time):
+                yield Ping, OUT, recipient_node_id, self._next_version()
+            self.channels_out.add(recipient_node_id)
+            self.last_seen[recipient_node_id] = current_time
+            self.queues.setdefault(recipient_node_id, []).append(msg_handle)
+
+    def sig_disconnect_received(self, sender_node_id):
+        if sender_node_id in self.channels_in:
+            self.channels_in.remove(sender_node_id)
+        elif sender_node_id in self.channels_out:
+            self.channels_out.remove(sender_node_id)
+        else:
+            return
+        yield NodeDown, sender_node_id
+        for msg_handle in self.queues.pop(sender_node_id, []):
+            yield SendFailed, msg_handle
+
+    def ping_received(self, dir, sender_node_id, version, current_time):
+        self.last_seen[sender_node_id] = current_time
+        if dir == IN:
+            self.channels_in.add(sender_node_id)
+        if dir == OUT:
+            self.channels_out.add(sender_node_id)
+        inout = (IN if sender_node_id in self.channels_in else OUT)
+        if self._needs_ping(sender_node_id, current_time):
+            yield Ping, inout, sender_node_id, self._next_version()
+        if sender_node_id in self.queues:
+            for msg_handle in self.queues.pop(sender_node_id):
+                yield Send, inout, sender_node_id, self._next_version(), msg_handle
+        else:
+            if not (version > self.versions.get(sender_node_id, 0)):
+                # version has been reset--he has restarted, so emulate a node-down-node-back-up event pair:
+                yield NodeDown, sender_node_id
+                self.versions[sender_node_id] = version
+                assert sender_node_id not in self.queues
+        self.versions[sender_node_id] = version
+
+    def message_received(self, dir, sender_node_id, version, msg_body_bytes, current_time):
+        yield self.ping_received(dir, sender_node_id, version, current_time)
+        if msg_body_bytes:
+            yield Receive, msg_body_bytes, sender_node_id
+
+    def heartbeat(self, current_time):
+        t_gone = current_time - self.heartbeat_max_silence
+        removed = set()
+        for node_id in chain(self.channels_in, self.channels_out):
+            if self.last_seen[node_id] < t_gone:
+                yield Disconnect, node_id
+                removed.add(node_id)
+                for msg_handle in self.queues.pop(node_id, []):
+                    yield SendFailed, msg_handle
+                yield NodeDown, node_id
+            else:
+                if self._needs_ping(node_id, current_time):
+                    yield Ping, (IN if node_id in self.channels_in else OUT), node_id, self._next_version()
+        self.channels_in -= removed
+        self.channels_out -= removed
+
+    def ensure_connected(self, node_id, current_time):
+        # open fresh channel
+        if node_id not in self.channels_in and node_id not in self.channels_out:
+            yield Connect, node_id
+            yield Ping, OUT, node_id, self._next_version()
+            self.last_seen[node_id] = current_time
+            self.channels_out.add(node_id)
+            self.queues[node_id] = []
+
+    def shutdown(self):
+        for node_id in chain(self.channels_in, self.channels_out):
+            yield SigDisconnect, (IN if node_id in self.channels_in else OUT), node_id
+            for msg_handle in self.queues.pop(node_id, []):
+                yield SendFailed, msg_handle
+        self.channels_in = self.channels_out = None
+
+    # private:
+
     def _needs_ping(self, node_id, t):
         ret = self.last_sent.get(node_id, 0) <= t - self.heartbeat_interval
         if ret:
             self.last_sent[node_id] = t
         return ret
 
-    def send_message(self, recipient_node_id, msg_handle, current_time):
-        # is it a new connection, or an existing but not yet active connection?
-        if recipient_node_id in self.channels:
-            if self._is_connection_active(recipient_node_id):
-                yield SendPacket, recipient_node_id, self._make_ping_packet() + msg_handle.serialise()
-            else:
-                self.queues[recipient_node_id].append(msg_handle)
-        else:
-            yield Connect, recipient_node_id
-            if self._needs_ping(recipient_node_id, current_time):
-                yield SendPacket, recipient_node_id, self._make_ping_packet()
-            self.channels.add(recipient_node_id)
-            self.last_seen[recipient_node_id] = current_time
-            self.queues.setdefault(recipient_node_id, []).append(msg_handle)
-
-    def message_received(self, sender_node_id, msg_bytes, current_time):
-        if msg_bytes == DISCONNECT_SIGNAL:
-            dbg("DISCONNECT RECEIVED")
-            if sender_node_id in self.channels:
-                yield Disconnect, sender_node_id
-                self.channels.remove(sender_node_id)
-                yield NotifyNodeDown, sender_node_id
-                for msg_handle in self.queues.pop(sender_node_id, []):
-                    yield NotifySendFailed, msg_handle
-        else:
-            version, msg_body_bytes = struct.unpack(MSG_HEADER_FORMAT, msg_bytes[:4]), msg_bytes[4:]
-            if sender_node_id not in self.channels:
-                self.channels.add(sender_node_id)
-                self.last_seen[sender_node_id] = current_time
-                if self._needs_ping(sender_node_id, current_time):
-                    yield SendPacket, sender_node_id, self._make_ping_packet()
-            else:
-                if sender_node_id in self.queues:
-                    for msg_handle in self.queues.pop(sender_node_id, []):
-                        yield SendPacket, sender_node_id, self._make_ping_packet() + msg_handle.serialise()
-                else:
-                    if not (version > self.versions.get(sender_node_id, 0)):
-                        # version has been reset--he has restarted, so emulate a node-down-node-back-up event pair:
-                        yield NotifyNodeDown, sender_node_id
-                        self.versions[sender_node_id] = version
-                        assert sender_node_id not in self.queues
-            self.versions[sender_node_id] = version
-            self.last_seen[sender_node_id] = current_time
-            if msg_body_bytes:
-                yield Deliver, msg_body_bytes, sender_node_id
-
-    def heartbeat(self, current_time):
-        t_gone = current_time - self.heartbeat_max_silence
-        removed = set()
-        for node_id in self.channels:
-            if self.last_seen[node_id] < t_gone:
-                yield Disconnect, node_id
-                removed.add(node_id)
-                #
-                for msg_handle in self.queues.pop(node_id, []):
-                    yield NotifySendFailed, msg_handle
-                yield NotifyNodeDown, node_id
-            else:
-                if self._needs_ping(node_id, current_time):
-                    yield SendPacket, node_id, self._make_ping_packet()
-        self.channels -= removed
-
-    def watch_new_node(self, node_id, current_time):
-        # open fresh channel
-        yield Connect, node_id
-        yield SendPacket, node_id, self._make_ping_packet()
-        self.last_seen[node_id] = current_time
-        self.channels.add(node_id)
-        self.queues[node_id] = []
-
-    def shutdown(self):
-        for node_id in self.channels:
-            yield SendPacket, node_id, DISCONNECT_SIGNAL
-            for msg_handle in self.queues.pop(node_id, []):
-                yield NotifySendFailed, msg_handle
-            yield NotifyNodeDown, node_id
-        self.channels.clear()
-
-    # private:
-
-    def _make_ping_packet(self):
+    def _next_version(self):
         self.version += 1
-        return struct.pack(MSG_HEADER_FORMAT, self.version)
+        return self.version
 
-    def _is_connection_active(self, node_id):
-        return node_id in self.versions
+    def __repr__(self):
+        return "HubLogic()"
