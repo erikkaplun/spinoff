4183b9aada0f69e7402e3c7910d7fbe9529e7d26
Erik Allik <eallik@gmail.com>
2013-02-13 12:34:12 +0100

    More compact repr of values returned from enumrange/enums

--- spinoff/util/python.py
+++ spinoff/util/python.py
@@ -39,7 +39,7 @@ class EnumValue(object):
         return str(self.name)
 
     def __repr__(self):
-        return '<EnumValue %s%s>' % (self.name, ':%s' % self.order if self.order is not None else '')
+        return '<%s%s>' % (self.name, ':%s' % self.order if self.order is not None else '')
 
 
 def enums(*names):
