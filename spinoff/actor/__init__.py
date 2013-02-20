3ca07b10e2cad3c1a2483b743c7c8b68cc8edda1
Erik Allik <eallik@gmail.com>
2013-02-20 04:29:45 +0200

    Don't export Ref from spinoff.actor

--- spinoff/actor/__init__.py
+++ spinoff/actor/__init__.py
@@ -1,8 +1,8 @@
 from ._actor import Actor
 from .node import Node
-from .ref import Ref
+# from .ref import Ref
 from .uri import Uri
 from .props import Props
 
 
-__all__ = [Actor, Node, Ref, Uri, Props]
+__all__ = [Actor, Node, Uri, Props]
