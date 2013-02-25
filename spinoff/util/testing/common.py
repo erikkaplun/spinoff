7a2844edc72bd3a726d0708449cb35f210187987
Erik Allik <eallik@gmail.com>
2013-02-25 21:50:45 +0200

    Cleaned s.u.testing.common

--- spinoff/util/testing/common.py
+++ spinoff/util/testing/common.py
@@ -56,50 +56,6 @@ def expect_one_warning(message=None):
 swallow_one_warning = expect_one_warning
 
 
-class MockFunction(object):
-
-    called = property(lambda self: len(self.argses))
-
-    def __init__(self, return_values=None):
-        self.reset()
-        self.return_values = return_values and list(return_values)
-
-    def __call__(self, *args, **kwargs):
-        self.argses.append(args)
-        self.kwargses.append(kwargs)
-        if self.return_values is not None:
-            return self.return_values.pop(0)
-
-    def assert_called(self, n=None, message=None):
-        if isinstance(n, basestring):
-            n, message = None, n
-        if n is not None:
-            assert self.called == n
-        else:
-            assert self.called
-        self.reset()
-
-    def assert_called_with_signature(self, *args, **kwargs):
-        args_, kwargs_ = self.argses.pop(0), self.kwargses.pop(0)
-        assert args == args_ and kwargs == kwargs_, "mock function called with different arguments than expected"
-
-    def reset(self):
-        self.argses = []
-        self.kwargses = []
-
-
-def errback_called(d):
-    mock_fn = MockFunction()
-    d.addErrback(mock_fn)
-    return mock_fn.called
-
-
-def callback_called(d):
-    mock_fn = MockFunction()
-    d.addCallback(mock_fn)
-    return mock_fn.called
-
-
 def timeout(timeout=None):
     def decorate(fn):
         fn.timeout = timeout
