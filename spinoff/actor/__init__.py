3c6a3e601a360f53204fac804a78ef1b082cb3cf
Erik Allik <eallik@gmail.com>
2013-02-25 21:38:13 +0200

    Export Unhandled directly from spinoff.actor

--- spinoff/actor/__init__.py
+++ spinoff/actor/__init__.py
@@ -3,6 +3,7 @@ from .node import Node
 # from .ref import Ref
 from .uri import Uri
 from .props import Props
+from .exceptions import Unhandled
 
 
-__all__ = [Actor, Node, Uri, Props]
+__all__ = [Actor, Node, Uri, Props, Unhandled]
