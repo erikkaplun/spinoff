2b8fa7a03588ab1d543e4c5afce5042bb6c47226
Erik Allik <eallik@gmail.com>
2013-05-13 22:01:18 +0300

    Added timeout to s.u.lockfile with a default value of 10 seconds

--- spinoff/util/lockfile.py
+++ spinoff/util/lockfile.py
@@ -1,20 +1,24 @@
 from __future__ import absolute_import
 
+import time
 from contextlib import contextmanager
 
 from gevent import sleep
-from lockfile import LockFile, AlreadyLocked
+from lockfile import LockFile, AlreadyLocked, LockTimeout
 
 
 @contextmanager
-def lock_file(path, maxdelay=.1, lock_cls=LockFile):
+def lock_file(path, maxdelay=.1, lock_cls=LockFile, timeout=10.0):
     """Cooperative file lock. Uses `lockfile.LockFile` polling under the hood.
 
     `maxdelay` defines the interval between individual polls.
 
     """
     lock = lock_cls(path)
+    max_t = time.time() + timeout
     while True:
+        if time.time() >= max_t:
+            raise LockTimeout("Timeout waiting to acquire lock for %s" % (path,))  # same exception messages as in lockfile
         try:
             lock.acquire(timeout=0)
         except AlreadyLocked:
