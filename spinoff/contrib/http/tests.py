8d2b0a397dfc0a96eb8d7566dca1de6ee3375746
Erik Allik <eallik@gmail.com>
2013-08-28 14:52:56 +0300

    Added tests for contrib.http

---
+++ spinoff/contrib/http/tests.py
@@ -0,0 +1,88 @@
+import random
+import tempfile
+
+import requests
+from gevent.event import AsyncResult
+from gevent.monkey import patch_all
+from nose.tools import eq_
+
+from spinoff.actor import Node, Actor
+from spinoff.util.python import deferred_cleanup
+from spinoff.contrib.http.server import HttpServer
+
+
+patch_all()  # to make python-requests cooperative
+
+
+@deferred_cleanup
+def test_basic(defer):
+    node = Node()
+    defer(node.stop)
+
+    responders = []
+    http_server = node.spawn(HttpServer.using(address=('localhost', 0), responders=responders))
+    _, port = actor_exec(node, lambda: http_server.ask('get-addr'))
+
+    eq_(404, requests.get('http://localhost:%d' % (port,)).status_code)
+
+    rnd_response = str(random.random())
+    responders[:] = [
+        (r'^/$', make_responder(lambda request: request.write(rnd_response))),
+    ]
+    eq_(200, requests.get('http://localhost:%d' % (port,)).status_code)
+
+
+@deferred_cleanup
+def test_urlmatch_with_extra_args_to_responder(defer):
+    node = Node()
+    defer(node.stop)
+
+    responder = make_responder(lambda req, extra: req.write(str(extra)))
+    rnd_data = bytes(random.random())
+    responders = [(r'', responder.using(extra=rnd_data))]
+    http_server = node.spawn(HttpServer.using(address=('localhost', 0), responders=responders))
+    _, port = actor_exec(node, lambda: http_server.ask('get-addr'))
+
+    eq_(rnd_data, requests.get('http://localhost:%d' % (port,)).text)
+
+
+@deferred_cleanup
+def test_file_upload(defer):
+    node = Node()
+    defer(node.stop)
+
+    def handle_file(request):
+        request.write(request.files['file'].read())
+
+    responders = [(r'^/handle-file$', make_responder(handle_file))]
+    http_server = node.spawn(HttpServer.using(address=('localhost', 0), responders=responders))
+    _, port = actor_exec(node, lambda: http_server.ask('get-addr'))
+
+    rnd_data = bytes(random.random())
+    with tempfile.NamedTemporaryFile() as f:
+        f.write(rnd_data)
+        f.flush()
+        req = requests.post('http://localhost:%d/handle-file' % (port,), files={'file': open(f.name, 'rb')})
+    eq_(200, req.status_code)
+    eq_(rnd_data, req.text)
+
+
+def make_responder(fn):
+    fn.__name__ = 'run'
+
+    class Responder(Actor):
+        def run(self, *args, **kwargs):
+            return fn(*args, **kwargs)
+    return Responder
+
+
+def actor_exec(node, fn, *args, **kwargs):
+    ret = AsyncResult()
+
+    class ExecActor(Actor):
+        def run(self):
+            ret.set(fn(*args, **kwargs))
+
+    node.spawn(ExecActor)
+
+    return ret.get()
